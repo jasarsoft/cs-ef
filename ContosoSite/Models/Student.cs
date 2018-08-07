459229ff99d7021bdf966934310afc3360f15ac4
Edin Jašarević <edinjasar14@gmail.com>
2018-08-07 22:32:23 +0200

    anotacija podataka

--- ContosoSite/Models/Student.cs
+++ ContosoSite/Models/Student.cs
@@ -11,7 +11,8 @@ namespace ContosoSite.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Student
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,9 +22,16 @@ namespace ContosoSite.Models
         }
     
         public int StudentID { get; set; }
+
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [StringLength(50)]
         public string FirstName { get; set; }
+
         public Nullable<System.DateTime> EnrollmentDate { get; set; }
+
+        [StringLength(50)]
         public string MiddleName { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
